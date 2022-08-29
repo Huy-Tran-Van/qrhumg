2c93a81f9070b616375cf2008934b1a352f70ade
Văn Huy Trần <huy1821051049@gmail.com>
2022-08-30 01:07:18 +0700

    update

--- pages/InfrastructureDetail/[index].js
+++ pages/InfrastructureDetail/[index].js
@@ -9,15 +9,15 @@ import { useRouter } from 'next/router'
 import StyledInfratructureDetail from "../../components/InfrastructureDetail/styled";
 
 const Detail = ({ dataNew }) => {
-  const router = useRouter()
-  console.log("router", router);
-  if (typeof window !== 'undefined') {
-    var hostname = window.location.host;
-    console.log("hostname", hostname);
-  }
+  // const router = useRouter()
+  // console.log("router", router);
+  // if (typeof window !== 'undefined') {
+  //   var hostname = window.location.host;
+  //   console.log("hostname", hostname);
+  // }
   return (
     <>
-      <SEO
+      {/* <SEO
         title={dataNew?.attributes?.Title}
         urlKey={`${hostname}/InfrastructureDetail/${dataNew?.attributes?.slug}`}
         // image={`http://localhost:1337/${dataNew?.attributes?.image?.data[0].attributes?.url}`}
@@ -28,19 +28,19 @@ const Detail = ({ dataNew }) => {
       <Menu />
       <StyledInfratructureDetail.WrapperDetail>
         <InfoDetail dataNew={dataNew} />
-      </StyledInfratructureDetail.WrapperDetail>
+      </StyledInfratructureDetail.WrapperDetail> */}
+      <div>huy</div>
     </>
   );
 };
 
 export default Detail;
 
-export async function getServerSideProps(context) {
-  const params = context.query.index;
-  const res = await axios.get(`${LOCALHOST_URL_API_STRAPI}/api/news?populate=*&filters[slug]=${params}`)
-  // const dataNew = await res.json()
-  console.log("dataNew", res.data.data);
-  return {
-    props: { dataNew: res.data.data[0] }, // will be passed to the page component as props
-  };
-}
+// export async function getServerSideProps(context) {
+//   const params = context.query.index;
+//   const res = await axios.get(`${LOCALHOST_URL_API_STRAPI}/api/news?populate=*&filters[slug]=${params}`)
+//   console.log("dataNew", res.data.data);
+//   return {
+//     props: { dataNew: res.data.data[0] }, // will be passed to the page component as props
+//   };
+// }
