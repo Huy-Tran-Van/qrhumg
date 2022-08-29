3663326ac70f9628961f36cdf5c4c9afc793f737
Văn Huy Trần <huy1821051049@gmail.com>
2022-08-29 22:37:19 +0700

    update code

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    forceSwcTransforms: true,
-  },
-  disableExperimentalFeaturesWarning: true,
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  swcMinify: true,
 }
 
 module.exports = nextConfig
