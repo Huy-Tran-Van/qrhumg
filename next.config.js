a09e56a7351d881b2e61e01e5e6326c7ecdc3da9
Văn Huy Trần <huy1821051049@gmail.com>
2022-08-29 22:26:50 +0700

    update code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
+  disableExperimentalFeaturesWarning: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
 
