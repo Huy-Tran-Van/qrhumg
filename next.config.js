c67253a3739872cf63294430e6da5efbe58c7383
Văn Huy Trần <huy1821051049@gmail.com>
2022-09-06 16:55:34 +0700

    update code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  useFileSystemPublicRoutes: false,
   swcMinify: true,
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
   eslint: {
