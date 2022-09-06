5cdc8f1c1a49cbb5b00a4bcbfdac450510d2d2d9
Văn Huy Trần <huy1821051049@gmail.com>
2022-09-06 10:49:32 +0700

    update code

--- ultis/index.js
+++ ultis/index.js
@@ -1,4 +1,4 @@
 export const URL_API = "http://localhost:1337/api/"
 export const LOCALHOST_URL_API = "http://localhost:3000"
 export const LOCALHOST_URL_API_STRAPI = "http://localhost:1337"
-export const URL_SERVER_WEB = "https://qrhumg-i1c3s42ti-huy-tran-van.vercel.app"
+export const URL_SERVER_WEB = "qrhumg.vercel.app"
